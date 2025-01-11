7e5dd0afa510acdc2bcbdd421f023af00db03e33
olganow <olganow@yandex.ru>
2025-01-11 13:01:09 +0300

    parameters added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,11 +18,11 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = BASE_URL;
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browserSize");
         Configuration.timeout = 10000;
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
